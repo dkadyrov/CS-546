488453f434db90a8b5a3b3c3348ae45d63a11da9
unknown <daniel.kadyrov@gmail.com>
2019-06-13 14:20:36 -0400

    lab2

--- Lectures/lecture_02/lab_02/objUtils.js
+++ Lectures/lecture_02/lab_02/objUtils.js
@@ -1,4 +1,49 @@
-function extend(...args) { 
-    // This method will take the properties from earlier objects in the array `args`, and compose a new object with the combined property from all the entries **without** overwriting properties from earlier entries.
+function checkObject(object){ 
+    // Check if variable is object
+    if (typeof object!=="object") { 
+        throw `${object || "provided variable"} is not an object`;
+    }
+    // check if variable
+
 
 }
+
+
+function extend(...args) { 
+    // This method will take the properties from earlier objects in the array `args`, and compose a new object with the combined property from all the entries **without** overwriting properties from earlier entries.
+    // console.log(args)
+    for (let i of args) { 
+
+    }
+
+    let object = Object.assign({}, ...args.reverse())
+    // for (let i of args.reverse()) { 
+    //     object = Object.assign({}, i);
+    // }
+
+    return object
+}
+
+const first = {
+    x: 2,
+    y: 3
+};
+const second = {
+    a: 70,
+    x: 4,
+    z: 5
+};
+const third = {
+    x: 0,
+    y: 9,
+    q: 10
+};
+
+const firstSecondThird = extend(first, second, third);
+console.log(firstSecondThird)
+
+const secondThird = extend(second, third);
+console.log(secondThird)
+
+const thirdFirstSecond = extend(third, first, second);
+console.log(thirdFirstSecond)
