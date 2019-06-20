8266304a6ebae9bd4447c688714e11af5bca254f
unknown <daniel.kadyrov@gmail.com>
2019-06-20 15:00:58 -0400

    more lab3

--- Lectures/lecture_03/lab_03/index.js
+++ Lectures/lecture_03/lab_03/index.js
@@ -4,25 +4,37 @@ function checkIndex(object, index){
     if (typeof index !== "number") { 
         throw `${index || "index"} is not a number`;
     }
+    if (index < 0) {
+        throw `${index || "index"} is less than 0`;
+    }
     if (index > Object.keys(object).length){
         throw `${index || "index"} is greater than the length`;
-    }
-    if (index < 0){ 
-        throw `${index || "index"} is less than 0`;
     }
 }
 
 function checkString(string){
+    if (string === undefined){
+        throw "Undefined variable(s)"
+    }
     if (typeof string !== "string") {
         throw `${string || "string"} is not a string`;
     }
 }
 
-// const bluebird = require("bluebird");
-// const Promise = bluebird.Promise;
-
 async function getPeople(){
     const { data } = await axios.get("https://gist.githubusercontent.com/robherley/5112d73f5c69a632ef3ae9b7b3073f78/raw/24a7e1453e65a26a8aa12cd0fb266ed9679816aa/people.json");
+
+    return data
+}
+
+async function getWork() {
+    const { data } = await axios.get("https://gist.githubusercontent.com/robherley/61d560338443ba2a01cde3ad0cac6492/raw/8ea1be9d6adebd4bfd6cf4cc6b02ad8c5b1ca751/work.json");
+
+    return data 
+}
+
+async function getWeather() { 
+    const { data } = await axios.get("https://gist.githubusercontent.com/robherley/1b950dc4fbe9d5209de4a0be7d503801/raw/eee79bf85970b8b2b80771a66182aa488f1d7f29/weather.json")
 
     return data
 }
@@ -83,30 +95,71 @@ async function firstNameMetrics(){
     return metrics
 }
 
+async function shouldTheyGoOutside(firstName, lastName){
+    checkString(firstName)
+    checkString(lastName)
+    const people = await getPeople();
+
+    const person = people.find(i => i.firstName == firstName && i.lastName == lastName);
+
+    if (person === undefined) {
+        throw `${person || "person"} cannot be found`;
+    }
+
+    const weather = await getWeather();
+
+    const temp = weather.find(i => i.zip == person.zip).temp;
+
+    let status = "Yes"
+    let status1 = "should"
+    if (temp < 34){
+        status = "No"
+        status1 = "should not"
+    }
+
+    return `${status}, ${person.firstName} ${status1} go outside.`
+}
+
 async function whereDoTheyWork(firstName, lastName){
     checkString(firstName)
     checkString(lastName)
     const people = await getPeople();
-    const person = people.find(i => i.firstName == firstName && i.lastName == lastName)
 
-    if (person !== "undefined") {
-        
+    const person = people.find(i => i.firstName == firstName && i.lastName == lastName);
+
+    if (person === undefined) {
+        throw `${person || "person"} cannot be found`;
     }
 
+    const work = await getWork();
 
+    const job = work.find(i => i.ssn == person.ssn);
 
-    return person
+    let fired = "will not"
+    if (job.willBeFired === "true"){
+        fired = "will"
+    }
 
-    // }
+    const line = `${person.firstName} ${person.lastName} - ${job.jobTitle} at ${job.company}. They ${fired} be fired.`
 
-    // const work = await axios.get("https://gist.githubusercontent.com/robherley/61d560338443ba2a01cde3ad0cac6492/raw/8ea1be9d6adebd4bfd6cf4cc6b02ad8c5b1ca751/work.json");
+    return line
+}
+
+async function findTheHacker(ip){
+    // checkIP(ip)
+    const work = await getWork();
+
+    const person_ip = work.find(i => i.ssn == ip);
+
+    const people = await getPeople();
+
+    console.log(work)
 
 }
 
 async function main() {
     // x = await lexIndex(2)
-    console.log(await whereDoTheyWork("Demetra", "Durrand"));
+    console.log(await findTheHacker(123));
 }
-// function shouldTheyGoOutside(firstname, lastname){
 main()
 // }
